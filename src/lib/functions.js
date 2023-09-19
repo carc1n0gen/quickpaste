1c48df7d06495201f8399c50ce4cd7113fdf7dbc
Carson Evans <carson_evans@manulife.ca>
2023-09-19 16:07:29 -0400

    use origin header when behind reverse proxy

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,8 +1,6 @@
 export function getBaseUrl(req) {
-  const protocol = req.headers.get("X-Forwarded-Proto");
-  const host = req.headers.get("Host");
   if (process.env.REVERSE_PROXY) {
-    return `${protocol}://${host}`;
+    return request.headers.get("origin");
   }
 
   const url = new URL(req.url);
